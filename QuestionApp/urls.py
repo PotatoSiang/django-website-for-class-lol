8b4d26840677eb5a9f90f4603179cb2ea11cd951
PotatoSiang <potatosiang2008@gmail.com>
2021-12-02 20:36:51 +0800

    sned help

--- QuestionApp/urls.py
+++ QuestionApp/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
 from .views import HomepageView, QuestionView
-
 urlpatterns = [
     path('question/<int:pk>/', QuestionView.as_view(), name='question_detail'),
     path('', HomepageView.as_view(), name='home')
