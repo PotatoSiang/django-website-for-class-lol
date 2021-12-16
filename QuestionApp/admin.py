dfdf1adcf3a9c8caa02682590c3000a62bbb8de8
PotatoSiang <potatosiang2008@gmail.com>
2021-12-16 20:15:07 +0800

    AE

--- QuestionApp/admin.py
+++ QuestionApp/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 
-from QuestionApp.models import Choice, Question, Vote
+from .models import Choice, Question, Vote, Comment
 
 # Register your models here.
-class ChoiceInLine(admin.TabularInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 1
 class QuestionAdmin(admin.ModelAdmin):
-    inlines=[ChoiceInLine]
+    inlines=[ChoiceInline]
     list_display = ('question_text', 'pub_date')
 
 admin.site.register(Question, QuestionAdmin)
@@ -16,14 +16,15 @@ admin.site.register(Question, QuestionAdmin)
 class ReadOnlyMixin:
     def has_add_permission(self, request):
         return False
-'''
     def has_change_permission(self, request, obj=None):
         return False
     def has_delete_permission(self, request, obj=None):
         return False
-'''
+    
 class VoteAdmin(ReadOnlyMixin, admin.ModelAdmin):
     list_display = ('voter_name', 'vote_date', 'choice')
-
-
 admin.site.register(Vote, VoteAdmin)
+
+class CommentAdmin(ReadOnlyMixin, admin.ModelAdmin):
+    pass
+admin.site.register(Comment, CommentAdmin)
