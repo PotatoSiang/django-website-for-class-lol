68f9fcba7cd2c412204b60319c8a1c6444ce2e2b
PotatoSiang <potatosiang2008@gmail.com>
2021-12-16 20:26:00 +0800

    oh god

--- QuestionApp/admin.py
+++ QuestionApp/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 
-from .models import Choice, Question, Vote, Comment
+from QuestionApp.models import Choice, Comment, Question, Vote
 
 # Register your models here.
-class ChoiceInline(admin.TabularInline):
+class ChoiceInLine(admin.TabularInline):
     model = Choice
     extra = 1
 class QuestionAdmin(admin.ModelAdmin):
-    inlines=[ChoiceInline]
+    inlines=[ChoiceInLine]
     list_display = ('question_text', 'pub_date')
 
 admin.site.register(Question, QuestionAdmin)
@@ -16,15 +16,19 @@ admin.site.register(Question, QuestionAdmin)
 class ReadOnlyMixin:
     def has_add_permission(self, request):
         return False
+'''
     def has_change_permission(self, request, obj=None):
         return False
     def has_delete_permission(self, request, obj=None):
         return False
-    
+'''
 class VoteAdmin(ReadOnlyMixin, admin.ModelAdmin):
     list_display = ('voter_name', 'vote_date', 'choice')
+
+
 admin.site.register(Vote, VoteAdmin)
 
-class CommentAdmin(ReadOnlyMixin, admin.ModelAdmin):
+class CommentAdmin(ReadOnlyAdminMixin, admin.ModelAdmin):
     pass
+
 admin.site.register(Comment, CommentAdmin)
--- QuestionApp/views.py
+++ QuestionApp/views.py
@@ -1,68 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy, reverse
-from django.utils import timezone
+from django.views.generic import ListView, DetailView
 
-from .models import Question, Comment
+from .models import Question
 
 # Create your views here.
 class HomepageView(ListView):
     model = Question
     template_name = 'QuestionApp/home.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['comment_list'] = Comment.objects.all()
-        return context
-
 class QuestionView(DetailView):
     model = Question
     template_name = 'QuestionApp/detail.html'
-
-class CommentAddView(CreateView):
-    model = Comment
-    template_name = 'QuestionApp/comment_add.html'
-    fields = ['comment_title', 'comment_content']
-
-    def form_valid(self, form):
-        form.instance.user_name = 'test_user'
-        form.instance.comment_date = timezone.now()
-        return super().form_valid(form)
-
-class CommentDetailView(DetailView):
-    model = Comment
-    template_name = 'QuestionApp/comment_detail.html'
-
-class CommentUpdateView(UpdateView):
-    model = Comment
-    template_name = 'QuestionApp/comment_add.html'
-    fields = ['comment_title', 'comment_content']
-
-class CommentDeleteView(DeleteView):
-    model = Comment
-    template_name = 'QuestionApp/comment_del.html'
-    def get_success_url(self):
-        return reverse_lazy('home')
-
 '''
 def homepage_view(request):
     q_list = Question.objects.all()
     context = {'question_list':q_list}
-    return render(request,'QuestionApp/home.html', context)
-    '''
+    return render(request, 'QuestionApp/home.html', context)
+'''
 '''
 def homepage_view(request):
     q_list = Question.objects.all()
     if q_list:
-        content = '<br>'.join(['<li>'+q.question_text+'</li>' for q in q_list])
+        content = '<br>'.join (['<li>'+q.question_text+'<li>' for q in q_list])
+
     else:
         content = r'There is no question yet.'
-    html = r'<!DOCTYPE html> <html><head><title>'+\
+    html = r'<DOCTYPE html> <html><head><title>'+\
         r'my investigation Questionnaire'+\
         r'</title></head><body>'+\
         r'<h1>my investigation Questionnaire</h1>'+\
         r'<ol>'+content+r'<ol>'+\
         r'</body></html>'
     return HttpResponse(html)
-    '''
+'''
